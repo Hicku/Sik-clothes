9ab901210dfd03feb75ecfe66427cf73f5e3c72b
unknown <aaron.hickman201@gmail.com>
2024-05-21 17:07:02 +0100

    page setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,27 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Homepage from "./Pages/Homepage";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+import Navabar from "./Components/Navbar";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <Router>
+      <div className="App">
+        <header>
+          <Navabar />
+        </header>
+        <main className="main-container-app">
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Register />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
