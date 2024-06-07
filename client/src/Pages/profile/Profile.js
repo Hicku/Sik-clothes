82e70c1e76ff3905bc7450a16e24663b13d90e75
unknown <aaron.hickman201@gmail.com>
2024-06-07 11:25:03 +0100

    profile styles

--- client/src/Pages/profile/Profile.js
+++ client/src/Pages/profile/Profile.js
@@ -1,5 +1,44 @@
+import "./profile.css";
+
 function profile() {
-  return <div>Profile</div>;
+  const userData = JSON.parse(localStorage.getItem("user"));
+
+  return (
+    <div className="profile-container">
+      <section>
+        <ul className="account-options-list">
+          <li>
+            <div>Username: {userData.name}</div>
+          </li>
+          <li>
+            <div>Account</div>
+          </li>
+          <li>
+            <div>Order history</div>
+          </li>
+          <li>
+            <div>Wishlist</div>
+          </li>
+          <li>
+            <div>Account details</div>
+          </li>
+          <li>
+            <div>Change password</div>
+          </li>
+          <li>
+            <div>Contact</div>
+          </li>
+          <li>
+            <div>Address details</div>
+          </li>
+          <li>
+            <div>Payment details</div>
+          </li>
+        </ul>
+      </section>
+      <section className="details-container"></section>
+    </div>
+  );
 }
 
 export default profile;
