8a7d1e5b0eb26562f87988ef677d06bd8db0a6b3
unknown <aaron.hickman201@gmail.com>
2024-06-08 18:15:51 +0100

    display profile components

--- client/src/Pages/profile/Profile.js
+++ client/src/Pages/profile/Profile.js
@@ -7,46 +7,45 @@ import Contact from "../../Components/contact/Contact";
 import OrderHistory from "../../Components/orderHistory/OrderHistory";
 import PaymentDetails from "../../Components/paymentDetails/PaymentDetails";
 import Wishlist from "../../Components/wishlist/Wishlist";
+const { useState } = require("react");
 
 function Profile() {
   const userData = JSON.parse(localStorage.getItem("user"));
+
+  const [currentComponent, setCurrentComponent] = useState("account");
+
+  const handleComponentChange = (component) => {
+    setCurrentComponent(component);
+  };
+
+  const components = {
+    account: <Account />,
+    "account details": <AccountDetails />,
+    "address details": <AddressDetails />,
+    "change password": <ChangePassword />,
+    contact: <Contact />,
+    "order History": <OrderHistory />,
+    "payment details": <PaymentDetails />,
+    wishlist: <Wishlist />,
+  };
 
   return (
     <div className="profile-container">
       <section>
         <ul className="account-options-list">
-          <li>
-            <button>Username: {userData.name}</button>
-          </li>
-          <li>
-            <button>Account</button>
-          </li>
-          <li>
-            <button>Order history</button>
-          </li>
-          <li>
-            <button>Wishlist</button>
-          </li>
-          <li>
-            <button>Account details</button>
-          </li>
-          <li>
-            <button>Change password</button>
-          </li>
-          <li>
-            <button>Contact</button>
-          </li>
-          <li>
-            <button>Address details</button>
-          </li>
-          <li>
-            <button>Payment details</button>
-          </li>
+          {Object.keys(components).map((component) => {
+            return (
+              <li>
+                <button onClick={() => handleComponentChange(component)}>
+                  {component}
+                </button>
+              </li>
+            );
+          })}
         </ul>
       </section>
       <section className="details-container">
-        {/* <Account /> */}
-        <AccountDetails />
+        {components[currentComponent]}
       </section>
     </div>
   );
