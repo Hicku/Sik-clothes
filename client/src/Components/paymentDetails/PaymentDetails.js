432cb5bb19247322b6896f73cab37bd303ac439a
unknown <aaron.hickman201@gmail.com>
2024-06-20 14:13:15 +0100

    payment styles

--- client/src/Components/paymentDetails/PaymentDetails.js
+++ client/src/Components/paymentDetails/PaymentDetails.js
@@ -1,5 +1,19 @@
+import "./paymentDetails.css";
+import { FaRegCreditCard } from "react-icons/fa";
+
 function paymentDetails() {
-  return <div>Payment</div>;
+  return (
+    <div className="payment-details-container">
+      <section className="add-card-container">
+        <div>
+          <FaRegCreditCard className="add-card-icon" />
+        </div>
+        <div>
+          <button className="add-card-button">Add card</button>
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default paymentDetails;
